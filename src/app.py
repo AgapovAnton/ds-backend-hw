0e009cb806dd0b7c4dd7895255548a2a140a1722
AgapovAnton <agapovanton@mail.ru>
2023-03-22 08:15:25 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -1,13 +1,69 @@
-from flask import Flask
+from flask import Flask, request
+from models.plate_reader import PlateReader
 import logging
+import io
+from PIL import UnidentifiedImageError
+import requests
 
 
 app = Flask(__name__)
+plate_reader = PlateReader.load_from_file('./model_weights/plate_reader_model.pth')
+URL_IMAGES = 'http://51.250.83.169:7878/images/'
 
 
 @app.route('/')
 def hello():
-    return '<h1><center>Hello!</center></h1>'
+    return '<h1><center>Hello!<?center><?h1>'
+
+
+# ip:port/toUpper?s=hello
+@app.route('/toUpper')
+def to_upper():
+    if 's' not in request.args:
+        return 'invalid args', 400
+
+    s = request.args['s']
+    return {
+        'result': s.upper(),
+    }
+
+
+# /readNumber <- img bin
+# {"name": "о128но11"}
+@app.route('/readNumber', methods=["POST"])
+def read_number():
+    body = request.get_data()
+    im = io.BytesIO(body)
+    try:
+        res = plate_reader.read_text(im)
+    except UnidentifiedImageError:
+        return {"error": "invalid image"}, 400
+    return {"name": res}
+
+
+# ip:port/ID?s <- img binary
+@app.route('/ID')
+def id():
+    if 's' not in request.args:
+        return 'invalid args', 400
+
+    id_img = request.args['s']
+
+    url_curr_img = URL_IMAGES + id_img + ".jpg"
+
+    p = requests.get(url_curr_img, stream=True)
+    out = open(url_curr_img, "wb")
+    body = out.write(p.content)
+
+    im = io.BytesIO(body)
+    out.close()
+    try:
+        res = plate_reader.read_text(im)
+    except UnidentifiedImageError:
+        return {"error": "invalid image"}, 400
+    
+    return {"name": res}
+
 
 
 if __name__ == '__main__':
@@ -16,4 +72,5 @@ if __name__ == '__main__':
         level=logging.INFO,
     )
 
+    app.config['JSON_AS_ASCII'] = False
     app.run(host='0.0.0.0', port=8080, debug=True)
